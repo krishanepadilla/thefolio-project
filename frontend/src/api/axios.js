a591a3663309d451b8ea58479b0349019c32d4d3
Shane <kpadilla23108533@student.dmmmsu.edu.ph>
2026-04-10 07:52:05 +0800

    Modify baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,8 +4,8 @@
 
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api' 
 });
 
 // Attach JWT token to every request automatically
